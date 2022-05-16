53ad69aa20900c3ce8ba9b6ff79199f323058869
lucastan96 <lucastan96@outlook.com>
2022-05-17 00:03:05 +0100

    Project commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchMoviesHandler = useCallback(async () => {
+        setIsLoading(true);
+        setError(null);
+
+        try {
+            const response = await fetch("https://react-a378a-default-rtdb.europe-west1.firebasedatabase.app/movies.json");
+            if (!response.ok) {
+                throw new Error("Something went wrong.");
+            }
+
+            const data = await response.json();
+            const loadedMovies = [];
+
+            for (const key in data) {
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate
+                });
+            }
+
+            setMovies(loadedMovies);
+            setIsLoading(false);
+        } catch (e) {
+            setError(e.message);
+            setIsLoading(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchMoviesHandler();
+    }, [fetchMoviesHandler]);
+
+    const addMovieHandler = async movie => {
+        const response = await fetch("https://react-a378a-default-rtdb.europe-west1.firebasedatabase.app/movies.json", {
+            method: "POST",
+            body: JSON.stringify(movie),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        await response.json();
+        fetchMoviesHandler();
+    };
+
+    let content = <p>No movies yet.</p>;
+
+    if (movies.length > 0) {
+        content = <MoviesList movies={movies} />;
+    }
+
+    if (error) {
+        content = <p>{error}</p>;
+    }
+
+    if (isLoading) {
+        content = <p>Loading...</p>
+    }
+
+    return (
+        <React.Fragment>
+            <section>
+                <AddMovie onAddMovie={addMovieHandler} />
+            </section>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>
+                {content}
+            </section>
+        </React.Fragment>
+    );
 }
 
 export default App;
